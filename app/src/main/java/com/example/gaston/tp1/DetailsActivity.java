f8ff751a28f0ab409dadc69526e4f94a33247827
patoGarcia1 <32282624+patoGarcia1@users.noreply.github.com>
2017-12-08 00:38:46 -0300

    Se agrega botón de retroceso en DetailsActivity, y correcciones menores

--- app/src/main/java/com/example/gaston/tp1/DetailsActivity.java
+++ app/src/main/java/com/example/gaston/tp1/DetailsActivity.java
@@ -1,13 +1,19 @@
 package com.example.gaston.tp1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    Button btnLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,17 @@ public class DetailsActivity extends AppCompatActivity {
         nombreTv.setText(post.getTitulo());
         TextView precioTv = (TextView) findViewById(R.id.cuerpo);
         precioTv.setText(post.getCuerpo());
+
+
+        btnLogo = (Button) findViewById(R.id.btnLogo);
+
+        btnLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(DetailsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
